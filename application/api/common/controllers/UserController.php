61765385d2093a04e3bf80470dc22649748fd5e7
vishnupriyam <mvpnov1994@gmail.com>
2015-11-11 12:37:05 +0530

    Moving the action times to version v1 user_controller

--- application/api/common/controllers/UserController.php
+++ application/api/common/controllers/UserController.php
@@ -2,8 +2,7 @@
 
 namespace api\common\controllers;
 use \Yii as Yii;
-use api\common\models\BlockTime;
-use Yii\db;
+
 
 class UserController extends \api\components\ActiveController
 {
@@ -19,29 +18,4 @@ class UserController extends \api\components\ActiveController
             ]
         ];
     }
-
-	/**
-	*	@POST method
-	*   POST Parameters - mobile_id of the requesting user
-	*   @return the block times added after the user's last access time
-	**/
-	public function actionTimes(){
-
-		$bodyParams = Yii::$app->getRequest()->getBodyParams();
-
-		$mobile_id = $bodyParams['mobile_id'];
-
-		$modelClassUser = '\api\versions\v1\models\User';
-		$User = $modelClassUser::find()->where(['mobile_id'=>$mobile_id])->one();
-
-		$modelClassUserCourses = '\api\versions\v1\models\UserHasCourse';
-		$courses_id = $modelClassUserCourses::find()->where(['user_id'=>$User['id']])->asArray()->all();
-
-		$modelClassTimes = '\api\versions\v1\models\BlockTime';
-		$Times = $modelClassTimes::find()->where(['course_id'=>$courses_id])->andWhere(['between','created_time',$User['access_time'],date('Y:m:d H:i:s')])->asArray()->all();
-
-		return $Times;
-
-	}
-
 }
--- application/api/versions/v1/controllers/UserController.php
+++ application/api/versions/v1/controllers/UserController.php
@@ -1,10 +1,37 @@
 <?php
 
 namespace api\versions\v1\controllers;
+use \Yii as Yii;
+
 
 class UserController extends \api\common\controllers\UserController
 {
 
 	public $modelClass = '\api\versions\v1\models\User';
+
+	/**
+	*	@POST method
+	*   POST Parameters - mobile_id of the requesting user
+	*   @return the block times added after the user's last access time
+	**/
+	public function actionTimes(){
+
+		$bodyParams = Yii::$app->getRequest()->getBodyParams();
+
+		$mobile_id = $bodyParams['mobile_id'];
+
+		$modelClassUser = '\api\versions\v1\models\User';
+		$User = $modelClassUser::find()->where(['mobile_id'=>$mobile_id])->one();
+
+		$modelClassUserCourses = '\api\versions\v1\models\UserHasCourse';
+		$courses_id = $modelClassUserCourses::find()->where(['user_id'=>$User['id']])->asArray()->all();
+
+		$modelClassTimes = '\api\versions\v1\models\BlockTime';
+		$Times = $modelClassTimes::find()->where(['course_id'=>$courses_id])->andWhere(['between','created_time',$User['access_time'],date('Y:m:d H:i:s')])->asArray()->all();
+
+		return $Times;
+
+	}
+
 
 }
