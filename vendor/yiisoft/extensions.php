b8d4d92f22b7c42df5b125ecf992bb3c43841eb3
vishnupriyam <mvpnov1994@gmail.com>
2015-11-11 19:42:35 +0530

    Adding vendor files

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,16 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'filsh/yii2-oauth2-server' => 
+  array (
+    'name' => 'filsh/yii2-oauth2-server',
+    'version' => '2.0.1.0',
+    'alias' => 
+    array (
+      '@filsh/yii2/oauth2server' => $vendorDir . '/filsh/yii2-oauth2-server',
+    ),
+    'bootstrap' => 'filsh\\yii2\\oauth2server\\Bootstrap',
+  ),
+);
